4b05af7531bf50b286c1e8520a2cbb88eb854eec
405516_GONZALEZ <405516@tecnicatura@frc.utn.edu.ar>
2024-03-23 17:12:30 -0300

    correcion ejercicio 2

--- src/main/java/org/example/App2.java
+++ src/main/java/org/example/App2.java
@@ -4,20 +4,28 @@ public class App2 {
 
         public static void main(String[] args)
         {
-            System.out.println("Seleccione una opcion:\n"+
+            System.out.println(
+                    "Seleccione una opcion:\n"+
                     "Opcion 1: calcular el precio del articulo con IVA\n"+
                     "Opcion 2 :Salir"
             );
             Scanner scanner=new Scanner(System.in);
-            double opcion=scanner.nextInt();
-            do {
-                System.out.println("Ingrese el precio del articulo a la venta");
-                Scanner scan=new Scanner(System.in);
-                double numeroIngresado= scanner.nextDouble();
-                double precioConIva=numeroIngresado*1.19;
-                System.out.println("El precio con un tasa fija de 19% es de: "+precioConIva);
-            }while (opcion>2);
-            System.out.println("Ingrese una opcion valida");
+            int opcion=scanner.nextInt();
+            switch (opcion){
+                case 1:
+                    System.out.println("Ingrese el precio del articulo a la venta");
+                    Scanner scan=new Scanner(System.in);
+                    double numeroIngresado= scanner.nextDouble();
+                    double precioConIva=numeroIngresado*1.19;
+                    System.out.println("El precio con un tasa fija de 19% es de: "+precioConIva);
+                    break;
+                case 2:
+                    System.out.println("Saliendo..");
+                    break;
+                default:
+                    System.out.println("Opcion invalida");
+            }
+            System.out.println("Programa finalizado");
 
         }
 
